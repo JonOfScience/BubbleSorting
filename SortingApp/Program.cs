f7c40fc8aaf20bf2fc12bd507b0a21c757274577
JonOfScience <jdcrofts31@gmail.com>
2022-07-20 08:47:32 +0100

    Add project files.

---
+++ SortingApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SortingApp;
+
+public class Sorter
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public int[] BubbleSort(int[] input)
+    {
+        return new int[] { -1 };
+    }
+}
