5542b2e52cebcd277a6ff7bbd83c0e2fa5ca90f6
JonOfScience <jdcrofts31@gmail.com>
2022-07-20 09:27:18 +0100

    BubbleSort Implementation

--- SortingApp/Program.cs
+++ SortingApp/Program.cs
@@ -9,6 +9,28 @@ public class Sorter
 
     public int[] BubbleSort(int[] input)
     {
-        return new int[] { -1 };
+        if (input.Length < 1) throw new ArgumentException("Input must contain at least one element");
+        if (input.Length == 1) return input;
+        if (input.All(x => x == input[0])) return input;
+
+        int changes;
+        int _temp;
+        do
+        {
+            changes = 0;
+            for (int idx = 0; idx < (input.Length - 1); idx++)
+            {
+                if (input[idx] > input[idx + 1])
+                {
+                    _temp = input[idx];
+                    input[idx] = input[idx + 1];
+                    input[idx + 1] = _temp;
+                    changes++;
+                }// If no changes can we start to step down the max value of idx?
+            }
+
+        } while (changes > 0);
+
+        return input;
     }
 }
